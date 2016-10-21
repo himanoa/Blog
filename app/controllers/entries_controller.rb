f98b8abeabbf560e34d98947a3c33222c5cf9a69
AC3ROZ <iori_16n1300006@nnn.ed.jp>
2016-10-21 17:15:12 +0900

    Rubocopで整形した

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -62,13 +62,14 @@ class EntriesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_entry
-      @entry = Entry.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def entry_params
-      params.require(:entry).permit(:title, :body_html, :body_md)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_entry
+    @entry = Entry.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def entry_params
+    params.require(:entry).permit(:title, :body_html, :body_md)
+  end
 end
--- test/controllers/entries_controller_test.rb
+++ test/controllers/entries_controller_test.rb
@@ -5,17 +5,17 @@ class EntriesControllerTest < ActionDispatch::IntegrationTest
     @entry = entries(:one)
   end
 
-  test "should get index" do
+  test 'should get index' do
     get entries_url
     assert_response :success
   end
 
-  test "should get new" do
+  test 'should get new' do
     get new_entry_url
     assert_response :success
   end
 
-  test "should create entry" do
+  test 'should create entry' do
     assert_difference('Entry.count') do
       post entries_url, params: { entry: { body_html: @entry.body_html, body_md: @entry.body_md, title: @entry.title } }
     end
@@ -23,22 +23,22 @@ class EntriesControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to entry_url(Entry.last)
   end
 
-  test "should show entry" do
+  test 'should show entry' do
     get entry_url(@entry)
     assert_response :success
   end
 
-  test "should get edit" do
+  test 'should get edit' do
     get edit_entry_url(@entry)
     assert_response :success
   end
 
-  test "should update entry" do
+  test 'should update entry' do
     patch entry_url(@entry), params: { entry: { body_html: @entry.body_html, body_md: @entry.body_md, title: @entry.title } }
     assert_redirected_to entry_url(@entry)
   end
 
-  test "should destroy entry" do
+  test 'should destroy entry' do
     assert_difference('Entry.count', -1) do
       delete entry_url(@entry)
     end
