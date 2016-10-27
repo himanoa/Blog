4707c8902e53c7a3795ffca834751414adbf6656
AC3ROZ <iori_16n1300006@nnn.ed.jp>
2016-10-27 13:36:27 +0900

    検索バーを追加した

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,7 +4,7 @@
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile << "unicode/*.png"
 Rails.application.config.assets.precompile += %w( skeleton.scss )
-Rails.application.config.assets.precompile += %w( oreore.scss )
+Rails.application.config.assets.precompile += %w( oreore.scss.erb )
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
